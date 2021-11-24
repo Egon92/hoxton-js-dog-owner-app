e7a9cac6805b963a370f137b3a31be7243aee24b
Egon <loliegon@gmail.com>
2021-11-24 21:40:56 +0100

    Added event click on main

--- src/index.js
+++ src/index.js
@@ -23,14 +23,14 @@ console.log(dogs);
 const dogsList = document.querySelector(`.dogs-list`)
 
 
-for (const dog of dogs){
+for(const dog of dogs){
     const dogsListItem = document.createElement(`li`)
     dogsListItem.setAttribute(`class`,`dogs-list__button`)
     dogsListItem.textContent = dog.name
     dogsList.append(dogsListItem)
 }
 
-for(const dog of dogs){
+function createDogCard (dog){
 
 const mainDogCard = document.querySelector(`.main__dog-section`)
 const dogName = document.createElement(`h2`)
@@ -50,18 +50,37 @@ dogAttributes.append(h3El, dogBio)
 
 const isGoodDog = document.createElement(`p`)
     isGoodDog.textContent =  `Is naughty?`
+    if(dog.isGoodDog){
+        isGoodDog.textContent =  `Is naughty? No!`
+        } else isGoodDog.textContent =  `Is naughty? Yes!`
+
 
 const dogButton = document.createElement(`button`)
     dogButton.setAttribute(`class`,`main__dog-section__btn`)
+    
+    if(dog.isGoodDog){
+    dogButton.textContent=`Bad dog`
+    } else dogButton.textContent=`Good dog`
 
-mainDogCard.append(dogName, dogImg, dogAttributes, isGoodDog, dogButton)
+
+    mainDogCard.append(dogName, dogImg, dogAttributes, isGoodDog, dogButton)
+
+    return mainDogCard
+
 }
 
+const mainDogCard = document.querySelector(`.main__dog-section`)
+const main = document.querySelector(`.main`)
 
-dogsListItem.addEventListener(`click`,function(event){
-        function(){
-            mainDogCard
-        }
-})
+const dogsListButtons = document.querySelectorAll(`.dogs-list__button`)
 
+for (let i = 0; i < dogsListButtons.length; i++) {
+    dogsListButtons[i].addEventListener('click', () => {
+        while (mainDogCard.firstChild)(
+            mainDogCard.removeChild(mainDogCard.firstChild)
+        )
+        main.append(createDogCard(dogs[i - 1]))
+            console.log(dogsListButtons)
+    })
+}
 
